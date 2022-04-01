94a8a190087e76a257361556f383c8a680e33758
PP Kun <40565728+PPKunOfficial@users.noreply.github.com>
2022-04-01 14:34:23 +0800

    源

---
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+        // Get the Intent that started this activity and extract the string
+        // 获得Intene的参数
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
