60ceb88c50e5b8df7d02472247bbbcc0a87a1f9c
PP Kun <40565728+PPKunOfficial@users.noreply.github.com>
2022-04-01 16:11:54 +0800

    添加注释

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.kt
@@ -9,11 +9,11 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
         // Get the Intent that started this activity and extract the string
-        // 获得Intene的参数
+        // 获得Intent的参数
         val message = intent.getStringExtra(EXTRA_MESSAGE) // 从intent中获取由Extra传递过来的数据(EXTRA_MESSAGE
 
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView).apply {
+        findViewById<TextView>(R.id.textView).apply {
             text = message
         }
     }
