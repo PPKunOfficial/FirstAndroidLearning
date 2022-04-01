df10dd3e1991353cda65fb36f85b2b9678ff9f83
PP Kun <40565728+PPKunOfficial@users.noreply.github.com>
2022-04-01 15:56:42 +0800

    添加注释

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,16 +10,17 @@ const val EXTRA_MESSAGE = "com.example.myapplication.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) // onCreate() 初始化Activity基本组件
+        setContentView(R.layout.activity_main) // 定义Activity布局，设置指定xml作为内容视图
     }
 
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+            putExtra(EXTRA_MESSAGE, message) // 方法将 EditText 的值添加到 intent。Intent 能够以称为“extra”的键值对形式携带数据类型。
+            // 当前键是一个公共常量 EXTRA_MESSAGE，因为下一个 activity 将使用该键检索文本值。为 intent extra 定义键时，最好使用应用的软件包名称作为前缀。这样可以确保这些键是独一无二的，这在您的应用需要与其他应用进行交互时会很重要。
         }
-        startActivity(intent)
+        startActivity(intent) // 方法将启动一个由 Intent 指定的 DisplayMessageActivity 实例
     }
 }
