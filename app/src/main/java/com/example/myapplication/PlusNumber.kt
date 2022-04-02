d6eaeb9184f22cb16b20671d25eefa8a7f585393
PP Kun <40565728+PPKunOfficial@users.noreply.github.com>
2022-04-02 23:41:20 +0800

    new activity

---
+++ app/src/main/java/com/example/myapplication/PlusNumber.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlusNumber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plus_number)
+    }
+}
